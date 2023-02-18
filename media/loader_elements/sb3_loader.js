b46c6996c1a6db47783ecb4cf45e378cf5c104e2
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2023-02-18 22:22:51 +0000

    See inside button

--- media/loader_elements/sb3_loader.js
+++ media/loader_elements/sb3_loader.js
@@ -2,13 +2,19 @@ const { searchParams: parameters } = new URL(location);
 
 const project = parameters.get("prj");
 const projectURL = `${location.host}/media/projects/${project}`;
+const other_params = [
+	[ "username", "Player" ],
+].map(e => `${e[0]}=${e[1]}`).join("&");
+
+const createURL = page => `https://turbowarp.org/${page}?project_url=${encodeURIComponent(projectURL)}&${other_params}`
 
 const projectEmbed = document.querySelector("#project");
-projectEmbed.src = `https://turbowarp.org/embed?project_url=${encodeURIComponent(projectURL)}`;
+projectEmbed.src = createURL("embed");
 
 const projectName = document.querySelector("#projectName"),
 	desc = document.querySelector("#description"),
-	instr = document.querySelector("#instructions");
+	instr = document.querySelector("#instructions"),
+	seeInside = document.querySelector("#seeInside");
 fetch("/media/loader_elements/projects.json")
 	.then(r => r.json())
 	.then(DATA => {
@@ -17,6 +23,7 @@ fetch("/media/loader_elements/projects.json")
 		document.title = `${projectData.name} - Projects`;
 		desc.innerText = projectData.desc;
 		instr.innerText = projectData.instr;
-	})
+		seeInside.href = createURL("editor");
+	});
 
 console.log(project);
