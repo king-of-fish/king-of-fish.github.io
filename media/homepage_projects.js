17d073b58aeeffb48edd9d70efee0d96619240fa
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2023-02-24 05:48:15 +0000

    Reformatting.

--- media/homepage_projects.js
+++ media/homepage_projects.js
@@ -3,11 +3,13 @@ fetch("/media/loader_elements/projects.json")
 	.then(r => r.json())
 	.then(projects => {
 		for (let project in projects) {
-			let listEl = document.createElement("li");
 			let link = document.createElement("a");
 			link.href = `/sb3_interface.html?prj=${project}`;
 			link.innerText = projects[project].name;
+
+			let listEl = document.createElement("li");
 			listEl.appendChild(link);
+
 			projectList.appendChild(listEl);
 		}
 	})
