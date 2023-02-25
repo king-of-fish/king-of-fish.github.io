0578a88d8168fcde72e56dc3c0632b9148726a25
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2023-02-25 14:43:02 -0800

    HTML reimplementation + code reorg

--- media/loader_elements/sb3_loader.js
+++ media/loader_elements/sb3_loader.js
@@ -2,6 +2,7 @@ const { searchParams: parameters } = new URL(location);
 
 const project = parameters.get("prj");
 const projectURL = `${location.host}/media/projects/${project}`;
+const EL = document.querySelector;
 const other_params = [
 	[ "username", "Player" ],
 	[ "addons", [ "pause" ].join(",") ],
@@ -13,26 +14,27 @@ const other_params = [
 
 const createURL = (page, overwritePrjURL = projectURL) => `https://turbowarp.org/${page}?project_url=${encodeURIComponent(overwritePrjURL)}&${other_params}`
 
-const projectEmbed = document.querySelector("#project");
+const projectEmbed = EL("#project");
 projectEmbed.src = createURL("embed");
 
-const projectName = document.querySelector("#projectName"),
-	desc = document.querySelector("#description"),
-	instr = document.querySelector("#instructions"),
-	seeInside = document.querySelector("#seeInside");
+const seeInside = EL("#seeInside");
+seeInside.href = createURL("editor");
+
+const projectName = EL("#projectName"),
+	desc = EL("#description"),
+	instr = EL("#instructions");
 fetch("/media/loader_elements/projects.json")
 	.then(r => r.json())
 	.then(DATA => {
-		let projectData = DATA[project];
+		let {useHTML, ...projectData} = DATA[project];
 		projectName.innerText = projectData.name;
 		document.title = `${projectData.name} - Projects`;
 		desc.innerText = projectData.desc;
-		if ("dHTML" in projectData)
+		if (useHTML.includes("desc"))
 			desc.innerHTML = projectData.desc;
 		instr.innerText = projectData.instr;
-		if ("dHTML" in projectData)
+		if (useHTML.includes("instr"))
 			instr.innerHTML = projectData.instr;
-		seeInside.href = createURL("editor");
 	});
 
-console.log(`Fetching project ${project}`);
+console.log(`Fetching project #{project}`);
